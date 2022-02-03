ab8ee4a56e62ff888900b6a922f92093e5b17b0a
immanuel-tran <Immanueltran@gmail.com>
2022-02-03 15:46:19 -0800

    Test Passes

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void multiplication() {
-        assertEquals(11, SkillDemo.multi(5, 2)); //fail at first
+        assertEquals(10, SkillDemo.multi(5, 2)); //fail at first
     }
 }
